2485c888fd6507e5ccd009f5f93eb748823b64d3
Eemeli <eemeli.myllymaki@live.fi>
2024-07-05 11:30:07 +0300

    Contact form on small screens

--- components/ContactInput.tsx
+++ components/ContactInput.tsx
@@ -19,7 +19,7 @@ export default function App() {
                 <div className="sm:flex text-xl sm:text-medium items-center">
                     <div className="justify-items-start">
                         <p className="py-2">Viesti *</p>
-                        <textarea required type="text" name="content" className="w-96 h-32 outline bg-gray-800 outline-1 outline-gray-600 text-gray-300 rounded-lg p-3 font-normal"></textarea>
+                        <textarea required type="text" name="content" className="w-full sm:w-96 h-32 outline bg-gray-800 outline-1 outline-gray-600 text-gray-300 rounded-lg p-3 font-normal"></textarea>
                     </div>
                     <div className="space-y-4 pt-2 sm:pt-0 sm:pl-4">
                         <div>
