554bdde97c51c58f79c718be59e02981d6957ee1
PohjanPojka <43803936+PohjanPojka@users.noreply.github.com>
2024-07-16 10:31:53 +0300

    Delete build directory

--- build/client/assets/about-7kwrClOA.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime-BWtM72Fx.js";/* empty css             */function p(){return t.jsx(t.Fragment,{})}export{p as default};
--- build/client/assets/links-g8vDfUWB.js
+++
@@ -1,1 +0,0 @@
-const e=()=>[{rel:"icon",href:"/favicon.png",type:"image/png"},{rel:"stylesheet",type:"text/css",href:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"}];export{e as links};
